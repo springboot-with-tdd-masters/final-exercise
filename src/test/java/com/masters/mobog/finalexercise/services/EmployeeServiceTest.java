e3bf666def0ef38d42e154fb1acf05eaa6392a6e
marionino.obog <marioo@magenic.com>
2022-06-01 18:35:28 +0800

    [final] write test for save employee

--- src/test/java/com/masters/mobog/finalexercise/services/EmployeeServiceTest.java
+++ src/test/java/com/masters/mobog/finalexercise/services/EmployeeServiceTest.java
@@ -1,5 +1,6 @@
 package com.masters.mobog.finalexercise.services;
 
+import com.masters.mobog.finalexercise.entities.Employee;
 import com.masters.mobog.finalexercise.repositories.EmployeeRepository;
 import com.masters.mobog.finalexercise.services.impl.EmployeeServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTest {
@@ -26,7 +34,24 @@ public class EmployeeServiceTest {
     @Test
     @DisplayName("should create employee")
     void shouldCreateEmployee() {
+        // given
+        Employee forSaving = new Employee();
+        forSaving.setFirstname("Jay");
+        forSaving.setLastname("Rock");
+        forSaving.setCreatedDate(LocalDateTime.now());
+        forSaving.setLastModifiedDate(LocalDateTime.now());
 
+        Employee stub = new Employee();
+        stub.setId(1L);
+        stub.setFirstname("Jay");
+        stub.setLastname("Rock");
+        stub.setCreatedDate(LocalDateTime.now());
+        stub.setLastModifiedDate(LocalDateTime.now());
+        when(repository.save(any(Employee.class))).thenReturn(stub);
+        // when
+        Employee actual = service.createEmployee(forSaving);
+        verify(repository).save(any(Employee.class));
+        assertNotNull(actual.getId());
     }
     @Test
     @DisplayName("should find employee")
