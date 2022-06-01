d070b6ccd2dd9e9a1cd4846c9a4fcf6cd69f62bf
marionino.obog <marioo@magenic.com>
2022-06-01 17:27:23 +0800

    [final] init commit

---
+++ src/main/java/com/masters/mobog/finalexcerise/FinalexceriseApplication.java
@@ -0,0 +1,13 @@
+package com.masters.mobog.finalexcerise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalexceriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalexceriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masters/mobog/finalexcerise/FinalexceriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masters.mobog.finalexcerise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalexceriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
