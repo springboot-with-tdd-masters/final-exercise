aab22d82b9af93971e8d95d0e78f16a0750cc6e5
marionino.obog <marioo@magenic.com>
2022-06-01 18:26:21 +0800

    [final] write test for service

---
+++ src/test/java/com/masters/mobog/finalexercise/services/SkillServiceTest.java
@@ -0,0 +1,8 @@
+package com.masters.mobog.finalexercise.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class SkillServiceTest {
+}
