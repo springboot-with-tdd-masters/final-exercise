ae71af54b8c30e2fb55b863b4c686749185d0556
marionino.obog <marioo@magenic.com>
2022-06-01 21:37:53 +0800

    [final] create test for get employee

--- src/main/java/com/masters/mobog/finalexercise/controllers/EmployeeController.java
+++ src/main/java/com/masters/mobog/finalexercise/controllers/EmployeeController.java
@@ -1,7 +1,11 @@
 package com.masters.mobog.finalexercise.controllers;
 
+import com.masters.mobog.finalexercise.dto.EmployeeRequest;
+import com.masters.mobog.finalexercise.entities.Employee;
 import com.masters.mobog.finalexercise.services.EmployeeService;
 import com.masters.mobog.finalexercise.services.SkillService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +18,14 @@ public class EmployeeController {
     public EmployeeController(EmployeeService employeeService, SkillService skillService){
         this.skillService = skillService;
         this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    Employee createEmployee(EmployeeRequest request){
+        return this.employeeService.createEmployee(request);
+    }
+    @GetMapping("/{id}")
+    Employee getEmployeeById(Long id){
+        return this.employeeService.findById(id);
     }
 }
