af027682d3394c16f453bd3aa83220c3c5d0a3dc
marionino.obog <marioo@magenic.com>
2022-06-01 18:13:24 +0800

    [final] write tests for employee repository

--- src/main/java/com/masters/mobog/finalexercise/entities/Skill.java
+++ src/main/java/com/masters/mobog/finalexercise/entities/Skill.java
@@ -1,6 +1,8 @@
 package com.masters.mobog.finalexercise.entities;
 
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 public class Skill {
@@ -8,6 +10,50 @@ public class Skill {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Long id;
+
+    @Column
+    private String description;
+
+    @Column
+    private Integer duration;
+
+    @Column
+    private LocalDate lastUsed;
+
+    @ManyToOne
+    private Employee employee;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public LocalDate getLastUsed() {
+        return lastUsed;
+    }
+
+    public void setLastUsed(LocalDate lastUsed) {
+        this.lastUsed = lastUsed;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
 
     public Long getId() {
         return id;
--- src/test/java/com/masters/mobog/finalexercise/repositories/SkillRepositoryTest.java
+++ src/test/java/com/masters/mobog/finalexercise/repositories/SkillRepositoryTest.java
@@ -1,10 +1,20 @@
 package com.masters.mobog.finalexercise.repositories;
 
+import com.masters.mobog.finalexercise.entities.Employee;
+import com.masters.mobog.finalexercise.entities.Skill;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 public class SkillRepositoryTest {
@@ -18,31 +28,135 @@ public class SkillRepositoryTest {
     @Test
     @DisplayName("should save skill")
     void shouldSaveSkill(){
+        Employee employee = new Employee();
+        employee.setFirstname("Scott");
+        employee.setLastname("Travis");
+        Skill skill1 = new Skill();
+        skill1.setDescription("Skill 1");
+        skill1.setDuration(3);
+        skill1.setLastUsed(LocalDate.now());
+        skill1.setEmployee(employee);
+
+        Skill saved = repository.save(skill1);
+
+        // when
+        Skill actual = entityManager.find(Skill.class, saved.getId());
+        assertNotNull(actual);
 
     }
     @Test
     @DisplayName("should update skill")
     void shouldUpdateSkill(){
+        Employee employee = new Employee();
+        employee.setFirstname("Scott");
+        employee.setLastname("Travis");
+        Skill skill1 = new Skill();
+        skill1.setDescription("Skill 1");
+        skill1.setDuration(3);
+        skill1.setLastUsed(LocalDate.now());
+        skill1.setEmployee(employee);
 
+        Skill saved = repository.save(skill1);
+
+        // when
+        Skill update = new Skill();
+        update.setId(saved.getId());
+        update.setDescription("Skill 2");
+        update.setDuration(update.getDuration());
+        update.setLastUsed(saved.getLastUsed());
+
+        Skill actual = repository.save(update);
+        assertEquals(actual.getId(), update.getId());
+        assertEquals(actual.getDescription(), "Skill 2");
     }
     @Test
     @DisplayName("should find by skill id")
     void shouldFindBySkillId(){
+        Employee employee = new Employee();
+        employee.setFirstname("Scott");
+        employee.setLastname("Travis");
+        Skill skill1 = new Skill();
+        skill1.setDescription("Skill 1");
+        skill1.setDuration(3);
+        skill1.setLastUsed(LocalDate.now());
+        skill1.setEmployee(employee);
 
+        Skill saved = entityManager.persist(skill1);
+
+        // when
+        Optional<Skill> actual = repository.findById(saved.getId());
+        assertTrue(actual.isPresent());
     }
     @Test
     @DisplayName("should find all without pagination")
     void shouldFindAllWithoutPagination(){
+        Employee employee1 = new Employee();
+        employee1.setFirstname("Jay");
+        employee1.setLastname("Rock");
 
+
+        entityManager.persist(employee1);
+
+        Skill skill = new Skill();
+        skill.setDuration(2);
+        skill.setDescription("Skill 1");
+        skill.setLastUsed(LocalDate.now());
+        skill.setEmployee(employee1);
+
+        Skill skill2 = new Skill();
+        skill2.setDuration(2);
+        skill2.setDescription("Skill 2");
+        skill2.setLastUsed(LocalDate.now());
+        skill2.setEmployee(employee1);
+        // when
+        Page<Employee> page = null;
+
+        assertEquals(List.of(skill, skill2), page.getContent());
     }
     @Test
     @DisplayName("should find all with pagination")
     void shouldFindAllWithPagination(){
+        Employee employee1 = new Employee();
+        employee1.setFirstname("Jay");
+        employee1.setLastname("Rock");
 
+
+        entityManager.persist(employee1);
+
+        Skill skill = new Skill();
+        skill.setDuration(2);
+        skill.setDescription("Skill 1");
+        skill.setLastUsed(LocalDate.now());
+        skill.setEmployee(employee1);
+
+        Skill skill2 = new Skill();
+        skill2.setDuration(2);
+        skill2.setDescription("Skill 2");
+        skill2.setLastUsed(LocalDate.now());
+        skill2.setEmployee(employee1);
+        // when
+        Page<Employee> page = null;
+
+        assertEquals(List.of(skill), page.getContent());
     }
     @Test
     @DisplayName("should delete skill")
     void shouldDeleteSkill(){
+        Employee employee = new Employee();
+        employee.setFirstname("Scott");
+        employee.setLastname("Travis");
+        Skill skill1 = new Skill();
+        skill1.setDescription("Skill 1");
+        skill1.setDuration(3);
+        skill1.setLastUsed(LocalDate.now());
+        skill1.setEmployee(employee);
 
+        Skill saved = entityManager.persist(skill1);
+
+        // when
+        repository.delete(saved);
+        // then
+        Optional<Skill> actual = repository.findById(saved.getId());
+        assertTrue(actual.isEmpty());
     }
 }
