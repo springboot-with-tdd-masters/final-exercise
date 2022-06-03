dc01f76c67777828e47eeb45162d26edd15db648
Reyzald Grezola <reyzald.grezola@ph.softvision.global>
2022-06-03 16:16:42 +0800

    added swagger/openapi

--- src/main/java/com/finalexam/skills/config/SecurityConfig.java
+++ src/main/java/com/finalexam/skills/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,50 +23,58 @@ import org.springframework.web.filter.CorsFilter;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	@Autowired
-	private UserDetailsService userService;
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+
+  @Autowired
+  private UserDetailsService userService;
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
 		http
-			.csrf().disable()
-			.anonymous().disable()
-			.authorizeRequests()
-			.anyRequest().authenticated();
-	}
-	
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
-	
-	@Bean
-	public DaoAuthenticationProvider authenticationProvider() {
-	    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-	    provider.setPasswordEncoder(encoder());
-	    provider.setUserDetailsService(userService);
-	    return provider;
+				.csrf().disable()
+				.anonymous().disable()
+				.authorizeRequests()
+				.anyRequest().authenticated();
 	}
 
-	@Bean
-	public PasswordEncoder encoder() {
-		return new BCryptPasswordEncoder();
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Bean
+  public DaoAuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+    provider.setPasswordEncoder(encoder());
+    provider.setUserDetailsService(userService);
+    return provider;
+  }
+
+  @Bean
+  public PasswordEncoder encoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public FilterRegistrationBean<CorsFilter> corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.addAllowedHeader("*");
+    config.addAllowedOrigin("*");
+    config.addAllowedMethod("*");
+    source.registerCorsConfiguration("/**", config);
+
+    FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<>(
+        new CorsFilter(source));
+    filter.setOrder(0);
+    return filter;
+  }
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**","/swagger-resources");
 	}
-	
-	@Bean
-	public FilterRegistrationBean<CorsFilter> corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedHeader("*");
-		config.addAllowedOrigin("*");
-		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config);
-		
-		FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<>(new CorsFilter(source));
-		filter.setOrder(0);
-		return filter;
-	}
+
+
 }
