ba9f6884e205c1c8008433bdf298325b39a5b708
James Rod Mantaring <jamesrod.mantaring@ph.softvision.global>
2022-06-03 14:02:00 +0800

    [james]: fix id generation

--- src/main/java/com/example/employee/domain/entities/AbstractDomain.java
+++ src/main/java/com/example/employee/domain/entities/AbstractDomain.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public abstract class AbstractDomain {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Long id;
 
     @CreatedDate
